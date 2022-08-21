6be81d9721722ce37b302980529cec616b6c3b57
tm-ahad <tm.ahad.07@gmail.com>
2022-08-21 22:56:30 +0600

    hashByRandomSecret and hashByRandomSecrets done.

--- encryptor/hash/hashByRandomSecret.ts
+++ encryptor/hash/hashByRandomSecret.ts
@@ -2,9 +2,9 @@ import encryptor from "../encryptor.ts";
 
 const hashByRandomSecret = async (hashingData) => {
     let randSecret = `${Math.round(Math.random()*52)}${Math.random()*52 % 2 === 0 ? 'l': 'r'}`;
-    return await encryptor(hashingData, randSecret)
+    return await encryptor(hashingData, randSecret);
 };
 
 for (let i = 0; i < 999; i++){
-    console.log(await hashByRandomSecret('Tahmid'))
+    console.log(await hashByRandomSecret('{"name": "Tahmid", "age": 9, email: "tm.ahad.bd@gmail.com"}'))
 }
---
+++ encryptor/hash/hashByRandomSecrets.ts
@@ -0,0 +1,17 @@
+import encryptor from "../encryptor.ts";
+
+const hashByRandomSecrets = async (hashingData) => {
+    let randSecrets = "";
+    let len = hashingData.length;
+
+    while (len !== 0){
+        let randSecret = `${Math.round(Math.random()*52)}${Math.random()*52 % 2 === 0 ? 'l': 'r'}`;
+        randSecrets = `${randSecrets} ${randSecret}`
+        len--;
+    }
+    return await encryptor(hashingData, randSecrets);
+};
+
+for (let i = 0; i < 999; i++){
+    console.log(await hashByRandomSecrets('{name: "Tahmid", age: 9, email: "tm.ahad.07@gmail.com"}'))
+}
